885839451b451ab14e224351025c19e8f92badfa
esmikle <elizabethsmikle4@gmail.com>
2019-04-09 16:38:13 -0400

    Deliverable 3 update

--- src/ca/sheridancollege/project/GoFishPlayer.java
+++ src/ca/sheridancollege/project/GoFishPlayer.java
@@ -2,6 +2,8 @@ package ca.sheridancollege.project;
 
 public class GoFishPlayer extends Player {
 
+    private int numOfSets;
+    
     public GoFishPlayer(String name) {
         super(name);
     }
@@ -13,8 +15,21 @@ public class GoFishPlayer extends Player {
     
     @Override
     public void addToScore(){
-        
+        this.score += 1;
     }
+    
+    public int getScore(){
+        return this.score;
+    }
+    
+    public void addToSets(){
+        numOfSets += 1;
+    }
+    
+    public int getSets(){
+        return numOfSets;
+    }
+    
     
     
     
